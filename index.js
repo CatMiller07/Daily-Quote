b1156fc2d341b84582b3a63ad61147eeac9900e3
catmiller07 <catmiller07@comcast.net>
2015-10-21 22:05:12 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    function getQuotes(){
+       // alert("Here I am");
+        $.ajax({
+                method:"POST",
+                   url:"http://www.swolebrain.com:4004/quote",
+               success:function(res){
+                   $(".quotebx").html("");
+                   $("h2").html("");
+                   $("h2").html("Topic: "+res.topic);
+                   $(".quotebx").html("<p>"+res.quote+"<br>"+"....."+res.author+"</p>");
+                   
+               },
+                 error:function(res){
+                       alert("you goofed!!");
+                   }
+        })
+        
+    }; 
+    
+     //Initialization
+    getQuotes();
+    
+    // Refresh Quotes when the button is pressed. 
+    $(".btn").click(getQuotes);
+});
